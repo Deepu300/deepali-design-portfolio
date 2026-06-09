6ac65316c6ceedd5efe5c304e5c65b2aeddfe1a1
Anurag Gharat <anuraggharat55@gmail.com>
2026-06-10 02:42:20 +0530

    warningn fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400..700&display=swap"
           rel="stylesheet"
